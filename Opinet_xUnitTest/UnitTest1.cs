49f9affc4182479e6ead08ea9783dabebee4e8ea
pjh2104 <ParkJungwhan@users.noreply.github.com>
2025-09-07 04:53:00 +0900

    병합 충돌 남은것 처리

--- Opinet_xUnitTest/UnitTest1.cs
+++ Opinet_xUnitTest/UnitTest1.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Configuration;
-=======
->>>>>>> origin/develop
 using OpinetScheduler.Services;
 
 namespace Opinet_xUnitTest
@@ -18,23 +14,18 @@ namespace Opinet_xUnitTest
             http.DefaultRequestHeaders.Add("Accept", "application/json");
             http.DefaultRequestHeaders.Add("User-Agent", "OpinetSchedulerTestClient/1.0");
 
-<<<<<<< HEAD
             IConfiguration config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .Build();
 
             opinet = new OpinetClient(http, config);
-=======
-            opinet = new OpinetClient(http);
->>>>>>> origin/develop
         }
 
         [Fact]
         public void Test1()
         {
             var result = opinet.GetAvgAllPriceAsync();
-<<<<<<< HEAD
             result.Wait();
 
             var sss = result.Result.RESULT.OIL;
@@ -56,8 +47,6 @@ namespace Opinet_xUnitTest
             result.Wait();
 
             var sss = result.Result.RESULT.OIL;
-=======
->>>>>>> origin/develop
         }
     }
 }
